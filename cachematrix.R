f6a498657ec55355040fbf62dc7b6dbbf9b8af31
AFOdom <anne@theodom.com>
2014-05-21 20:40:17 -0500

    Programming Asst 2
     Final cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,31 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# Given a matrix, return a list of functions for getting and setting
+# the inverse of that matrix.
 makeCacheMatrix <- function(x = matrix()) {
-        inverse <- NULL
-        #inverse <- solve(x)
-        get <- function() x
-        setInverse <- function(inverse) m <<- solve(x)
-        list (get=get,solvedMatrix = inverse, setInverse = setInverse)
-
+        m <- NULL
+        
+        # sets x equal to the argument of "set()" and m to NULL at the parent
+        # level, so the variables can be accessed by other child functions with
+        # the same parent.
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        get <- function() x # just returns x
+        
+        # assigns solve() function to m at the parent level, so m can be
+        # used by other functions
+        setinverse <- function(solve) m <<- solve 
+        
+        getinverse <- function() m # just returns m
+        
+        # puts all the functions we just created into a list and returns
+        # that list to give other functions access to them
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        inverse <- x$solvedMatrix
-        if(!is.null(inverse)) {
+# The x argument for cachesolve() should be the list of functions returned
+# by makeCacheMatrix(). cachesolve() will return the inverse of the matrix
+# cached in makeCacheMatrix().
+cachesolve <- function(x, ...) {
+       
+        # get the inverse matrix using the getinverse() function
+        # stored in x
+        m <- x$getinverse()
+        
+        # if m is not null, let the user know the function is getting
+        # cached data. Return the cached data. End the function.
+        if(!is.null(m)) {
                 message("getting cached data")
-                return(inverse)
+                return(m)
         }
-        data <- x$get
-        print(data)
-        inverse <- solve(data)
-        print(paste("inverse is",inverse, sep=""))
-        x$setInverse(inverse)
-        inverse
+       
+        # if m *is* null, assign the result of the get() function stored
+        # in x to get the original matrix passed into makeCacheMatrix().
+        data <- x$get()
+        
+        # get the inverse of that matrix
+        m <- solve(data)
+        
+        # use the setinverse() function in x to set the variable
+        # m to the inverse of data at the parent level, so all child functions
+        # of this parent have access to its contents.
+        x$setinverse(m)
+        
+        # return the inverse
+        m
 }
